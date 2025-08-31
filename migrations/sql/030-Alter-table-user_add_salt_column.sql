9d7cc58a60cefaf3284ffe2c997548bf98b61e4d
Aldis Adomavičius <aldisadom@gmail.com>
2025-08-31 10:22:00 +0300

    migrations fix

--- migrations/sql/030-Alter-table-user_add_salt_column.sql
+++ migrations/sql/030-Alter-table-user_add_salt_column.sql
@@ -1,7 +1,11 @@
 --liquibase formatted sql
 
 -- changeset Aldis:30
--- comment: Alter customers table add invoice number column
-ALTER TABLE users ADD COLUMN salt character varying(255)  NOT NULL;
+-- comment: Alter users table adding password salt
+ALTER TABLE users ADD COLUMN salt character varying(255);
 
+UPDATE users
+SET salt = '$2a$10$TaqaG43aDC1Ai75moe.ENO';
+
+ALTER TABLE users ALTER COLUMN salt SET NOT NULL;
 -- rollback ALTER TABLE users DROP COLUMN salt;
